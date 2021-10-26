19a260e30b7ddb6f093f402038dd694f84cb69f1
Sabathick <erickick13@gmail.com>
2021-10-26 18:58:44 -0300

    Day25 - eventcapture ok!

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/eventCapture.js
@@ -0,0 +1,17 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e){
+    e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
+
+button.addEventListener('click', () =>{
+    console.log('Click');
+},{
+    once: true
+});
